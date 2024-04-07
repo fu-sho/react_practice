c7a27a4f653ac8ce063750ebdfd11b45b1010173
fu-sho <155809835+fu-sho@users.noreply.github.com>
2024-04-07 23:49:41 +0900

    Add files via upload

---
+++ compornents/HomePage.js
@@ -0,0 +1,101 @@
+import React from 'react'
+import { Button } from 'react-bootstrap';
+import reactImage from "../Images/react.png";
+import jsImage from "../Images/js.png";
+import htmlcssImage from "../Images/htmlcss.png";
+import profileImage from "../Images/icon.jpg";
+
+
+const HomePage = () => {
+  return (
+    <div>
+        <div className="container text-center">
+      <h1>food_kami</h1>
+
+      <img src={profileImage} className="profileImage" />
+
+      <p>
+        food_kamiです。普段は独学でWeb系の学習を行っています。主にHTML/CSS/Javascript/Reactメインに取り扱っています。趣味は、ゲームやご飯巡りです。
+        美味しいところがあればSNSを通して発信を行っています。
+      </p>
+
+      <section class="page-section" id="services">
+        <div className="service">
+          <div class="text-center">
+            <h2 class="section-heading text-uppercase">PORTFOLIO</h2>
+            <h3 class="section-subheading text-muted mb-5">
+              私が作った作品一覧です
+            </h3>
+          </div>
+          <div class="row text-center">
+            <div class="col-md-4">
+              <span class="fa-stack fa-4x">
+                <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                <i class="fas fa-list-check fa-stack-1x fa-inverse"></i>
+              </span>
+              <h4 class="my-3">Todoリスト</h4>
+              <p class="text-muted">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
+                maxime quam architecto quo inventore harum ex magni, dicta
+                impedit.
+              </p>
+            </div>
+            <div class="col-md-4">
+              <span class="fa-stack fa-4x">
+                <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
+              </span>
+              <h4 class="my-3">レスポンシブサイト</h4>
+              <p class="text-muted">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
+                maxime quam architecto quo inventore harum ex magni, dicta
+                impedit.
+              </p>
+            </div>
+            <div class="col-md-4">
+              <span class="fa-stack fa-4x">
+                <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                <i class="fas fa-solid fa-calculator fa-stack-1x fa-inverse"></i>
+              </span>
+              <h4 class="my-3">電卓アプリ</h4>
+              <p class="text-muted">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
+                maxime quam architecto quo inventore harum ex magni, dicta
+                impedit.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="skill">
+        <div class="text-center">
+          <h1 class="title">スキル</h1>
+          <div class="row text-center">
+            <div class="col-md-4 services">
+              <img src={reactImage} />
+              <h4>React</h4>
+              <p>Reactがつかえます</p>
+            </div>
+            <div class="col-md-4 services">
+              <img src={htmlcssImage} />
+              <h4>HTML/CSS</h4>
+              <p>HTML/CSSがつかえます</p>
+            </div>
+            <div class="col-md-4 services">
+              <img src={jsImage} />
+              <h4>Javascript</h4>
+              <p>Javascriptがつかえます</p>
+            </div>
+          </div>
+          <button type="button" class="btn btn-primary">
+            スキル一覧
+          </button>
+        </div>
+      </section>
+    </div>
+    </div>
+  )
+}
+
+export default HomePage
